b9efb282dc06305145e773023304dfe5d028e3cd
mhagdorn <magnus.hagdorn@marsupium.org>
2021-09-10 16:03:58 +0100

    add a setup script

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+name = 'OptClim2'
+version = '0.1'
+release = '0.1.0'
+author = 'Magnus Hagdorn'
+
+setup(
+    name=name,
+    packages=find_packages(),
+    version=release,
+    include_package_data=True,
+    install_requires=[
+        'configobj',
+    ],
+    extras_require={
+        'lint': [
+            'flake8>=3.5.0',
+        ],
+        'testing': [
+            'pytest',            
+        ],
+    },
+    author=author,
+    description="OptClim2 optimisation framework for cylc workflows",
+)
